f79c8f2c6289cc8a26f24856fa0537e11affffc8
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-04-26 22:44:47 +0200

    Add sockets: join and remove

--- client/app.js
+++ client/app.js
@@ -1,5 +1,15 @@
 
-'use strict'
+const socket = io();
+
+socket.on('message', ({ author, content }) =>
+  addMessage(author, content))
+socket.on('join', ({ name }) =>
+  addMessage('Chat Bot', `${name} has joined the chat`)
+);
+socket.on('removeUser', ({ name }) =>
+  addMessage('Chat Bot', `${name} has left the chat`)
+);
+
 
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
@@ -22,6 +32,10 @@ function login(event) {
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
   }
+  socket.emit('join', {
+    name: userName,
+    id: socket.id
+  })
 };
 
 function addMessage(author, content) {
@@ -29,27 +43,33 @@ function addMessage(author, content) {
   message.classList.add('message');
   message.classList.add('message--received');
 
-  if (author === userName) {
+  if (author == userName) {
     message.classList.add('message--self');
+  } else if (author == "Chat Bot") {
+    message.classList.add('message--info');
   }
 
-  message.innerHTML = `
-    <h3 class="message__author">${author === userName ? 'You' : author}</h3>  
+  message.innerHTML = ` <h3 class="message__author">${author === userName ? 'You' : author}</h3>  
     <div class="message__content">
     ${content}
-    </div> 
-    `;
+    </div> `;
   messagesList.appendChild(message);
 }
 
 function sendMessage(event) {
   event.preventDefault();
 
-  if (messageContentInput.value == '') {
+  if (!messageContentInput.value) {
     alert('No message');
   } else {
     addMessage(userName, messageContentInput.value);
   }
+
+  socket.emit('message', {
+    author: userName,
+    content: messageContentInput.value
+  })
+
   messageContentInput.value = '';
 };
 
@@ -58,5 +78,5 @@ loginForm.addEventListener('submit', (event) => {
 });
 
 addMessageForm.addEventListener('submit', (event) => {
-  sendMessage(event);
+  sendMessage(event)
 });
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-
+const socket = require('socket.io');
 
 const app = express();
 const messages = [];
+let users = [];
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -15,6 +16,37 @@ app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
 });
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('join', user => {
+    users.push(user)
+    console.log(users)
+    socket.broadcast.emit('join', user);
+  })
+  socket.on('message', message => {
+    console.log('Oh, I\'ve got something from ' + socket.id)
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => {
+    userName = users.filter(user => user.id === socket.id)[0];
+    users = users.filter(user => user.id !== socket.id)
+
+    console.log(users);
+    console.log(userName);
+
+
+    socket.broadcast.emit('removeUser', userName);
+
+  });
+
+
+  console.log('I\'ve added a listener on message event \n');
+});
+
